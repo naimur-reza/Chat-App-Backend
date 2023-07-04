dbc111e103d56d4ad3d1b4e41c8c74e5abf08b99
Naimur Reza <naimur.rezaa@gmail.com>
2023-07-04 20:11:17 +0600

    done with a user connection methods methods

--- index.js
+++ index.js
@@ -11,7 +11,15 @@ const { Server } = require("socket.io");
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5000",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST"],
   },
+});
+
+io.on("connection", (socket) => {
+  console.log(`A new user connected ${socket.id}`);
+});
+
+server.listen(port, () => {
+  console.log(`listening on *:${port}`);
 });
