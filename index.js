3fe0a1db77a71d3d1365282514843ecda0806965
Naimur Reza <naimur.rezaa@gmail.com>
2023-07-04 19:55:22 +0600

    added socket data

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const http = require("http");
 const port = 5000;
 app.use(cors());
 app.use(express.json());
+
+const { Server } = require("socket.io");
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5000",
+    methods: ["GET", "POST"],
+  },
+});
