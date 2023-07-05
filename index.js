8b74290978ea05b06766236932e747a24a998ec1
Naimur Reza <naimur.rezaa@gmail.com>
2023-07-05 11:57:58 +0600

    link updated

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const { Server } = require("socket.io");
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://chat-app-client-sage.vercel.app",
     methods: ["GET", "POST"],
   },
 });
