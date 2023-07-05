b9ff16967a6f9953340eb0cb9a09577c03bc258b
Naimur Reza <naimur.rezaa@gmail.com>
2023-07-05 23:31:16 +0600

    done with send connected user notification in room

--- index.js
+++ index.js
@@ -5,22 +5,32 @@ const http = require("http");
 const port = 5000;
 
 app.use(cors());
+
 const { Server } = require("socket.io");
 
+let connectedUsers = [];
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://chat-app-client-sage.vercel.app",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST"],
+    credentials: true,
   },
 });
 
 io.on("connection", (socket) => {
   console.log(`A new user connected ${socket.id}`);
 
-  socket.on("join_room", (data) => {
-    console.log(data);
-    socket.join(data);
+  // Emit an event to inform all connected clients about the new user
+  connectedUsers.push(socket.id);
+  io.emit("user_connected", connectedUsers);
+
+  socket.on("join_room", ({ room, author }) => {
+    if (room && author) {
+      socket.join(room);
+      console.log(author);
+      io.emit("joined_user", { joinedRoom: room, author: author });
+    }
   });
 
   socket.on("send_message", (data) => {
